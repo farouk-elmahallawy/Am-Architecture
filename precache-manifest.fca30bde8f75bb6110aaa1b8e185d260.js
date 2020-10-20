6f7b5f0c3a09fa733a5476b6d153b6c3f325895a
Farouk <ro.2.a_2010@hotmail.com>
2020-10-20 16:52:05 +0200

    Updates

---
+++ precache-manifest.fca30bde8f75bb6110aaa1b8e185d260.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b4e5863657b9b57d86d95371db5be2",
+    "url": "/Am-Architecture/index.html"
+  },
+  {
+    "revision": "60f8ad702c369de1b258",
+    "url": "/Am-Architecture/static/css/main.e5891435.chunk.css"
+  },
+  {
+    "revision": "195ee256b9f6b7919057",
+    "url": "/Am-Architecture/static/js/2.74957bd9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Am-Architecture/static/js/2.74957bd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f8ad702c369de1b258",
+    "url": "/Am-Architecture/static/js/main.923f74bb.chunk.js"
+  },
+  {
+    "revision": "80d81691253bfb22eeba",
+    "url": "/Am-Architecture/static/js/runtime-main.f76f16e8.js"
+  },
+  {
+    "revision": "67a887c048d373c092d6923c151a6794",
+    "url": "/Am-Architecture/static/media/1.67a887c0.jpg"
+  },
+  {
+    "revision": "3d8721f531688ac0e7df1f90a078aa87",
+    "url": "/Am-Architecture/static/media/2.3d8721f5.jpg"
+  },
+  {
+    "revision": "625913d0228a5f629b4ddce35066cd28",
+    "url": "/Am-Architecture/static/media/3.625913d0.jpg"
+  },
+  {
+    "revision": "4c128964e5ada712a668cc14817ee8f0",
+    "url": "/Am-Architecture/static/media/4.4c128964.jpg"
+  },
+  {
+    "revision": "452b68e69c670970303fb0c34e1d51c1",
+    "url": "/Am-Architecture/static/media/basem.452b68e6.jpg"
+  },
+  {
+    "revision": "3be5193b9bd0a907691a112087b78df3",
+    "url": "/Am-Architecture/static/media/fd097614-892e-d7cb-c658-c05363e49c5d.3be5193b.jpg"
+  },
+  {
+    "revision": "a49251b2446096b3ef9f7b87650f159a",
+    "url": "/Am-Architecture/static/media/fff.a49251b2.jpg"
+  },
+  {
+    "revision": "9aeb310cbe11e61611e49b8d580c23ff",
+    "url": "/Am-Architecture/static/media/fv-min - Copy.9aeb310c.jpg"
+  },
+  {
+    "revision": "693152b7061f057898e25b988ce440ec",
+    "url": "/Am-Architecture/static/media/shahawy.693152b7.jpg"
+  }
+]);
