748346d670eb4f54f4f324fd87bf53f929b8f7a8
Farouk <ro.2.a_2010@hotmail.com>
2020-12-29 04:49:18 +0200

    Updates

--- precache-manifest.7496d78fb041aaf76d09e2bd45bda100.js
+++ precache-manifest.7496d78fb041aaf76d09e2bd45bda100.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "90d57ff04738be86250bcf9ba63d6cf1",
+    "revision": "ecd76fcf361dc3ff29146800a3f22287",
     "url": "/index.html"
   },
   {
-    "revision": "d59cf647b0a6d8761e26",
+    "revision": "84cc93f0d79638d53ae8",
     "url": "/static/css/main.cde96a7f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.74957bd9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d59cf647b0a6d8761e26",
-    "url": "/static/js/main.def7fd11.chunk.js"
+    "revision": "84cc93f0d79638d53ae8",
+    "url": "/static/js/main.557651bd.chunk.js"
   },
   {
     "revision": "d1fc75301f2224a4b358",
