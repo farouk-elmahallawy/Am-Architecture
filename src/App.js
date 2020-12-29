09a526c52640c1d868b3063a6f7a262883c2395e
Farouk <ro.2.a_2010@hotmail.com>
2020-12-29 04:53:14 +0200

    change in the app

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Ahmed from './components/about/ahmed.js'
 class App extends React.Component {
   render(){
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
           <Navbaricon/>
           <Switch>
