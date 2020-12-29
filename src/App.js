3259fad285dc7ba7dd955206de232867ef0b8f41
Farouk <ro.2.a_2010@hotmail.com>
2020-12-29 07:17:47 +0200

    fix bug with BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbaricon from './components/navbar/navbar.js'
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/home/home'
 import Samples from './components/samples/samples'
 import About from './components/about/about.js'
@@ -11,18 +11,18 @@ import Ahmed from './components/about/ahmed.js'
 class App extends React.Component {
   render(){
   return (
-    <HashRouter>
+    <BrowserRouter basename ="/directory-name">
       <div className="App">
           <Navbaricon/>
           <Switch>
-            <Route path ='/' exact component={Home}/>
-            <Route path = '/About_us' exact component= {About}/>
+            <Route exact path = '/' component={Home}/>
+            <Route exact path = '/About_us' component= {About}/>
             <Route path = '/Samples' component = {Samples }/>
             <Route path = '/About_us/Mostafa' component ={Mostafa}/>
             <Route path = '/About_us/Ahmed' component= {Ahmed} />
           </Switch>
       </div>
-    </HashRouter>
+    </BrowserRouter>
       );
 }
 }
